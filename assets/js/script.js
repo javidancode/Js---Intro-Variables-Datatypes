e957237cb32b7ae97988d60eb69be8acc67d7493
javidancode <javidanig@code.edu.az>
2022-08-18 00:48:04 +0400

    Added Js - Intro, Variables, Datatypes

---
+++ assets/js/script.js
@@ -0,0 +1,70 @@
+
+// var,let,const
+
+// var name = "Cavid"
+// var name ="Orxan"
+
+// let name1 = "Orxan"
+// let name2 = "Cavid"
+// const age = 18;
+
+// console.log(age);
+
+// console.log(name+ "  " +surname);
+// console.log(`${name} ${surname}`);
+
+
+// let age = 18;
+// let surname = "Qedirli"
+// let IsMarried = false;
+// let address = null;
+// let count;
+
+// let person1 = {
+//     name :"Cavidan",
+//     surname : "Qedirli",
+//     age : 18,
+//     phoneNumbers : [1522766,1522766]
+// }
+
+// let person2 = {
+//     name :"Orxan",
+//     surname : "Akberov",
+//     age : 24, 
+//     phoneNumbers : [1522766,1522766]
+// }
+
+// for (var i = 0; i < student.length; index++) {
+//     if (student[i].name == "Cavidan") {
+//         console.log(student[i]);
+//         break;
+//     }else{
+//         console.log("Notfound");
+//     }
+    
+// }
+
+// let student= [person1,person2 ];
+
+// for (let i = 0; i < student.length; i++); {
+
+//     for (let j = 0; j < student.length; i++) {
+        
+//         console.log(students[i].phoneNumbers[j]);
+        
+//     }
+   
+// } 
+
+// console.log(person.name + "-" person.surname);
+    
+// for (let i = 0; i < person1.phoneNumbers.length; i++) {
+        
+//     console.log(person1phoneNumbers[i]);
+// }
+
+// let numbers =[3,4,5,6,7,8];
+
+// numbers.forEach(item => {
+//     console.log(item);
+// });
